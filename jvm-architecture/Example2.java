1057279c24173c64f046d513757247c1b09f8450
Rohit Agarwal <programmingwithrohit@gmail.com>
2018-01-21 15:50:43 +0530

    Create Example2.java

---
+++ jvm-architecture/Example2.java
@@ -0,0 +1,17 @@
+package com.javamultiplex.jvm;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+
+		Student s1 = new Student();
+		Class c1 = s1.getClass();
+		Student s2 = new Student();
+		Class c2 = s2.getClass();
+		System.out.println(c1.hashCode());
+		System.out.println(c2.hashCode());
+		System.out.println(c1 == c2);// true
+
+	}
+
+}
